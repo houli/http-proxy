faea664f1c609728f5d44f1668ee643e9ad5abae
Eoin Houlihan <ehoulih@tcd.ie>
2016-02-21 15:36:30 +0000

    Cleanup HTTPS proxy

--- lib/proxy/https_proxy_plug.ex
+++ lib/proxy/https_proxy_plug.ex
@@ -5,27 +5,42 @@ defmodule Proxy.HttpsProxyPlug do
   def init(opts), do: opts
 
   def call(%Plug.Conn{method: "CONNECT"} = conn, _opts) do
-    client_sock = conn.adapter |> elem(1) |> elem(1)
-    {host, port} = parse_connect conn
-    sock = Socket.TCP.connect! host, port
-    Logger.info "Tunnel #{host}:#{port} opened"
-    client_sock |> Socket.Stream.send!("HTTP/1.1 200 Connection established\r\n\r\n")
-    t1 = Task.async(fn -> ssl_stream(client_sock, sock) end)
-    t2 = Task.async(fn -> ssl_stream(sock, client_sock) end)
-    tasks = Task.yield_many([t1, t2])
-    Enum.map(tasks, fn {task, _res} ->
-      Task.shutdown(task, :brutal_kill)
-    end)
-    Logger.info "Tunnel #{host}:#{port} closed"
-    %{conn | state: :sent}
+    conn
+    |> establish_connection
+    |> ssl_task_handle
+    |> Map.put(:state, :sent)
     |> halt
   end
-
   def call(conn, _opts), do: conn
 
   defp parse_connect(conn) do
     [host, port] = String.split(conn.request_path, ":")
     {host, String.to_integer(port)}
+  end
+
+  defp establish_connection(conn) do
+    {host, port} = parse_connect conn
+    Logger.info "Tunnel #{host}:#{port} opened"
+    conn
+    |> assign(:client_sock, conn.adapter |> elem(1) |> elem(1))
+    |> assign(:host, host)
+    |> assign(:port, port)
+    |> assign(:remote_sock, Socket.TCP.connect!(host, port))
+    |> send_chunked(200)
+  end
+
+  defp ssl_task_handle(conn) do
+    client = conn.assigns.client_sock
+    remote = conn.assigns.remote_sock
+    tasks = Task.yield_many([
+      Task.async(fn -> ssl_stream(client, remote) end),
+      Task.async(fn -> ssl_stream(remote, client) end)
+    ])
+    Enum.map(tasks, fn {task, _res} ->
+      Task.shutdown(task, :brutal_kill)
+    end)
+    Logger.info "Tunnel #{conn.assigns.host}:#{conn.assigns.port} closed"
+    conn
   end
 
   defp ssl_stream(sock1, sock2) do
